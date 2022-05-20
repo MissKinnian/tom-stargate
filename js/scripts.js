6eec2e659c460bde45a4b3a92fc16e7820a67819
Radoslav Bodó <bodik@cesnet.cz>
2022-05-20 10:24:08 +0200

    uvodni nastrel

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,21 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+function handle_irisform(event) {
+	event.preventDefault();
+	var user_input = $('#irisform input[name="password"]').val();
+
+	if (user_input == 'srsi') {
+		$('#irismodal').modal({keyboard: true});
+
+		$('#iriscontent').html($('#irismodal .modal-body').html());
+	} else {
+		$('#wrongpassword').modal({keyboard: true});
+	}
+}
+
+
+$(document).ready(function() {
+	$('#irisform').on('submit', handle_irisform);
+});
