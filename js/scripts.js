55cb3e921df59c0b882058eaa9f1b8877eef5dd8
Radoslav Bodó <bodik@cesnet.cz>
2022-05-22 10:59:06 +0200

    schvalene texty

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,9 @@
 
 function handle_irisform(event) {
 	event.preventDefault();
-	var user_input = $('#irisform input[name="password"]').val();
-
-	if (user_input == 'srsi') {
+	var user_input = $('#irisform input[name="password"]').val().toLowerCase();
+	if (user_input == String.fromCharCode(104,118,101,122,100,121,116,101,118,111,108,97,106,105,109,101,122,105,115,101,98,101)) {
 		$('#irismodal').modal({keyboard: true});
-
 		$('#iriscontent').html($('#irismodal .modal-body').html());
 	} else {
 		$('#wrongpassword').modal({keyboard: true});
